279f5745be792bea1f0f7fbc859f89f85ba568bc
Palii <paliydmn@gmail.com>
2024-08-03 12:07:37 +0300

    logger added, templates changed

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 # main.py
-from fastapi import FastAPI, HTTPException, Form, Request
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
-from fastapi.responses import StreamingResponse, RedirectResponse, JSONResponse
-from fastapi.staticfiles import StaticFiles
-from utils import get_ffprobe_data, construct_programs_dict, construct_ffmpeg_command
-from typing import Dict, List
-import subprocess
 import os
 import sys
-import atexit
-import signal
 import json
+import subprocess
+import signal
+import atexit
+import logging
+from logging.handlers import RotatingFileHandler
 from pathlib import Path
+from typing import Dict, List
 
+from fastapi import FastAPI, HTTPException, Form, Request
+from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+from utils import get_ffprobe_data, construct_programs_dict, construct_ffmpeg_command
 from models import AdapterConfig, Program, Stream, AvailableResources, SaveSelection
 
 CONFIG_LOG_FILE = "/var/log/ffmpeg_resi"
@@ -22,134 +24,123 @@ CONFIG_FILE_PATH = Path("adapters_config.json")
 # Setup Jinja2Templates
 templates = Jinja2Templates(directory="templates")
 
-# In-memory storage for adapter configurations (in a real application, use a database)
+# In-memory storage for adapter configurations / running processes
+# ToDo: (better to use database or file)
 adapters: Dict[int, AdapterConfig] = {}
+running_processes: Dict[int, subprocess.Popen] = {}
+
+# Configure logging
+log_path = "app.log"
+log_formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
+log_handler = RotatingFileHandler(log_path, maxBytes=5*1024*1024, backupCount=5)
+log_handler.setFormatter(log_formatter)
+log_handler.setLevel(logging.INFO)
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(log_handler)
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static/"), name="static")
+
 
 def save_adapters_to_file():
     """Save the current state of adapters to a JSON file."""
-    with open(CONFIG_FILE_PATH, 'w') as file:
+    logger.info("Saving adapters configuration to file.")
+    with CONFIG_FILE_PATH.open('w') as file:
         adapters_data = {k: v.dict() for k, v in adapters.items()}
         json.dump(adapters_data, file, indent=4)
 
+
 def load_adapters_from_file():
-    # Load the JSON data from the file
-    try:
-        if CONFIG_FILE_PATH.exists():
-            with open(CONFIG_FILE_PATH, 'r') as file:
-                adapters_data = json.load(file)
-                print("Loaded adapters data from JSON:",adapters_data)  # Debug print
-    except (FileNotFoundError, json.JSONDecodeError) as e:
-        print(f"Error loading adapters from file: {e}")
-
-    # Convert the loaded JSON data into a dictionary of AdapterConfig objects
-    for adapter_id_str, adapter_data in adapters_data.items():
+    """Load the adapters configuration from a JSON file."""
+    if CONFIG_FILE_PATH.exists():
         try:
-            adapter_id = int(adapter_id_str)  # Convert the adapter ID to an integer
-            
-            # Directly use the JSON data for programs and streams as dictionaries
-            programs = {}
-            for program_id_str, program_data in adapter_data.get("programs", {}).items():
-                program_id = int(program_id_str)
-                programs[program_id] = program_data
-            
-            # Create AdapterConfig object
-            adapter_config = AdapterConfig(
-                adapter_number=adapter_data["adapter_number"],
-                modulator_number=adapter_data["modulator_number"],
-                udp_url=adapter_data["udp_url"],
-                programs=programs,
-                running=adapter_data.get("running", False)
-            )
-            adapters[adapter_id] = adapter_config
-            print("Loaded adapters:\n\n", adapters[adapter_id])  # Debug print
-
-        except Exception as e:
-            print(f"Error parsing adapter data for adapter ID {adapter_id}: {e}")
-
-###
-#    selected_programs: Optional[Dict[int, Dict[str, List[str]]]] = None  # Store selected programs and streams
+            with CONFIG_FILE_PATH.open('r') as file:
+                adapters_data = json.load(file)
+                for adapter_id_str, adapter_data in adapters_data.items():
+                    adapter_id = int(adapter_id_str)
+                    programs = {int(prog_id): prog_data for prog_id, prog_data in adapter_data.get(
+                        "programs", {}).items()}
+                    adapters[adapter_id] = AdapterConfig(
+                        adapter_number=adapter_data["adapter_number"],
+                        modulator_number=adapter_data["modulator_number"],
+                        udp_url=adapter_data["udp_url"],
+                        programs=programs,
+                        running=adapter_data.get("running", False)
+                    )
+            logger.info("Loaded adapters configuration from file.")
+        except (FileNotFoundError, json.JSONDecodeError) as e:
+            logger.error(f"Error loading adapters from file: {e}")
 
 
-app = FastAPI()
-
-@app.get("/adapters/available", response_model=AvailableResources)
-def get_available_adapters():
-    print("Get available adapters") #
-    try:
-        # Run find command to list all modulators
-        output = subprocess.check_output("find /dev/dvb/ -type c -name 'mod*'", shell=True).decode('utf-8')
-        if output:
-            lines = output.strip().split('\n')
-            adapters = set()
-            modulators = set()
-
-            for line in lines:
-                parts = line.split('/')
-                if len(parts) >= 5:
-                    adapter = int(parts[3].replace("adapter", ""))
-                    modulator = int(parts[4].replace("mod", ""))
-
-                    adapters.add(adapter)
-                    modulators.add(modulator)
-
-            return {"adapters": sorted(adapters), "modulators": sorted(modulators)}
-        else:
-            return {"adapters": [], "modulators": []}
-    except subprocess.CalledProcessError as e:
-        print(f"Error: {e}")
-        return {"adapters": [], "modulators": []}
-
-
-# Todo:
 @app.on_event("startup")
 def on_startup():
+    logger.info("Starting application.")
     load_adapters_from_file()
+
 
 @app.on_event("shutdown")
 def on_shutdown():
+    logger.info("Shutting down application.")
     save_adapters_to_file()
 
-app.mount("/static", StaticFiles(directory="static/"), name="static")
-# In-memory storage for running processes
-running_processes: Dict[int, subprocess.Popen] = {}
+
+@app.get("/adapters/available", response_model=AvailableResources)
+def get_available_adapters():
+    """Fetch available adapters and modulators from the system."""
+    logger.info(f"Loading available adapters from {CONFIG_FILE_PATH} file.")
+    try:
+        output = subprocess.check_output(
+            "find /dev/dvb/ -type c -name 'mod*'", shell=True).decode('utf-8')
+        if output:
+            lines = output.strip().split('\n')
+            adapters = {
+                int(line.split('/')[3].replace("adapter", "")) for line in lines}
+            modulators = {
+                int(line.split('/')[4].replace("mod", "")) for line in lines}
+            return {"adapters": sorted(adapters), "modulators": sorted(modulators)}
+        return {"adapters": [], "modulators": []}
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Error fetching available adapters: {e}")
+        return {"adapters": [], "modulators": []}
 
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
+    logger.info("Get Root Request")
     return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.get("/adapters/", response_class=HTMLResponse)
 async def adapters_page(request: Request):
-    print(f"Adapters: {adapters}")
+    logger.info("Get Adapters page")
     return templates.TemplateResponse("adapters.html", {"request": request, "adapters": adapters})
+
+@app.get("/modulator", response_class=HTMLResponse)
+async def modulator(request: Request):
+    return templates.TemplateResponse("modulator.html", {"request": request})
 
 
 @app.get("/get_adapters/")
 def get_adapters():
-    print(f"GET Adapters: \n\n{adapters}")
     return adapters
 
 
 @app.post("/adapters/")
 def create_adapter(adapterConf: AdapterConfig):
-    print("CREATE ADAPTER =================================\n")
-    print(f"AdapterConfig: \n{adapterConf}")
     adapter_id = len(adapters) + 1
     adapters[adapter_id] = adapterConf
     save_adapters_to_file()
-    
-    print(f"CREATED NEW ADAPTER: {adapters[adapter_id]}")
+    logger.info(f"Adapter created: {adapterConf}")
     return {"id": adapter_id, "message": "Adapter created successfully"}
 
 
 @app.get("/adapters/{adapter_id}/scan")
 def scan_adapter(adapter_id: int):
-    print("SCAN =================================\n")
     if adapter_id not in adapters:
         raise HTTPException(status_code=404, detail="Adapter not found")
     adapter = adapters[adapter_id]
-    print(f"SCAN Adapter: \n\n {adapter}")
     ffprobe_data = get_ffprobe_data(adapter.udp_url)
 
     # Check if ffprobe_data is an error message or valid data
@@ -159,76 +150,74 @@ def scan_adapter(adapter_id: int):
 
     programs = construct_programs_dict(ffprobe_data)
     adapters[adapter_id].programs = programs
+    logger.info(f"Scanned adapter {adapter_id}: {len(programs)} programs found.")
     return {"programs": programs}
 
 
 @app.post("/adapters/{adapter_id}/start")
 def start_ffmpeg(adapter_id: int):
-    print("START =================================\n")
     if adapter_id in running_processes:
+        logger.warning(f"FFmpeg process is already running for adapter {adapter_id}.")
         raise HTTPException(
             status_code=400, detail="FFmpeg process is already running for this adapter")
     if adapter_id not in adapters:
+        logger.warning(f"Adapter {adapter_id} not found.")
         raise HTTPException(status_code=404, detail="Adapter not found")
 
     adapter = adapters[adapter_id]
-    print(f"adapter: \n\n{adapter}\n\n")
-
     # Construct the selected programs dictionary based on the 'selected' field
     selected_programs = {
         program_id: program.dict() for program_id, program in adapter.programs.items() if program.selected
     }
 
-    # Display the result
-    print(selected_programs)
-
-
-    print(f"selected_programs: \n\n{selected_programs}\n\n")
     adapter_log_file = f"{CONFIG_LOG_FILE}{adapter_id}.log"
     ffmpeg_cmd = construct_ffmpeg_command(
         adapter.udp_url, selected_programs, adapter.adapter_number, adapter.modulator_number, adapter_log_file)
-    print(f"FFmpeg command: {ffmpeg_cmd}")
+    logger.info(f"Starting FFmpeg for adapter {adapter_id} with command: {ffmpeg_cmd}")
     process = subprocess.Popen(ffmpeg_cmd, shell=True, preexec_fn=os.setsid)
     running_processes[adapter_id] = process
     # ToDo: check
     if process:
         adapter.running = True
-
     return {"message": "FFmpeg started"}
 
 
 @app.post("/adapters/{adapter_id}/stop")
 def stop_ffmpeg(adapter_id: int):
-    print("STOP =================================\n")
     if adapter_id not in running_processes:
+        logger.warning(f"FFmpeg process not found for adapter {adapter_id}.")
         raise HTTPException(status_code=404, detail="FFmpeg process not found")
 
     if not adapters[adapter_id].running:
+        logger.info(f"Adapter {adapter_id} is already stopped.")
         return {"message": "Adapter is already stopped"}
 
     process = running_processes[adapter_id]
     os.killpg(os.getpgid(process.pid), signal.SIGTERM)
     del running_processes[adapter_id]
     adapters[adapter_id].running = False
+    logger.info(f"Stopped FFmpeg for adapter {adapter_id}.")
     return {"message": "FFmpeg stopped"}
 
 
 @app.delete("/adapters/{adapter_id}/")
 def delete_adapter(adapter_id: int):
-    print("DELETE =================================\n")
     if adapter_id in running_processes:
-        raise HTTPException(
-            status_code=400, detail="Stop FFmpeg process before deleting the adapter")
+        logger.warning(f"Attempt to delete adapter {adapter_id} while FFmpeg is running.")
+        raise HTTPException(status_code=400, detail="Stop FFmpeg process before deleting the adapter")
     if adapter_id not in adapters:
+        logger.warning(f"Adapter {adapter_id} not found.")
         raise HTTPException(status_code=404, detail="Adapter not found")
 
     del adapters[adapter_id]
+    logger.info(f"Deleted adapter {adapter_id}.")
     return {"message": "Adapter deleted"}
 
 
 @app.post("/adapters/{adapter_id}/save")
 def save_selection(adapter_id: int, selection: SaveSelection):
     if adapter_id not in adapters:
+        logger.warning(f"Adapter {adapter_id} not found.")
         raise HTTPException(status_code=404, detail="Adapter not found")
 
     adapter = adapters[adapter_id]
@@ -241,7 +230,8 @@ def save_selection(adapter_id: int, selection: SaveSelection):
         for stream_type, streams in adapter.programs[program_id].streams.items():
             for i, stream in enumerate(streams):
                 if not isinstance(stream, Stream):
-                    adapter.programs[program_id].streams[stream_type][i] = Stream(**stream)
+                    adapter.programs[program_id].streams[stream_type][i] = Stream(
+                        **stream)
                 adapter.programs[program_id].streams[stream_type][i].selected = False
 
     # Update the adapter with selected channels and streams
@@ -256,27 +246,31 @@ def save_selection(adapter_id: int, selection: SaveSelection):
                         stream.selected = True
 
     save_adapters_to_file()
+    logger.info(f"Saved selection for adapter {adapter_id}.")
     # Respond with success message
     return {"message": "Selection saved successfully"}
 
 
 # Function to stop all ffmpeg processes
 def stop_ffmpeg_processes():
+    """Stop all running ffmpeg processes."""
     try:
         subprocess.run(['killall', 'ffmpeg'], check=True)
-        print("All ffmpeg processes have been stopped.")
+        for _, adapter in adapters.items():
+            adapter.running = False
+        logger.info("All ffmpeg processes have been stopped.")
     except subprocess.CalledProcessError as e:
-        print(f"Error stopping ffmpeg processes: {e}")
+        logger.error(f"Error stopping ffmpeg processes: {e}")
 
-# Register the function to be called on application exit
-atexit.register(stop_ffmpeg_processes)
 
-# Signal handler to stop ffmpeg processes on termination
 def signal_handler(sig, frame):
-    print(f"Received signal {sig}. Stopping all ffmpeg processes.")
+    """Handle termination signals."""
+    logger.info(f"Received signal {sig}. Stopping all ffmpeg processes.")
     stop_ffmpeg_processes()
     sys.exit(0)
 
-# Register signal handlers for SIGTERM and SIGINT
+
+# Register signal handlers and atexit function
 signal.signal(signal.SIGTERM, signal_handler)
 signal.signal(signal.SIGINT, signal_handler)
+atexit.register(stop_ffmpeg_processes)
--- utils.py
+++ utils.py
@@ -1,7 +1,26 @@
 # utils.py
 import subprocess
 import json
+import logging
 from typing import Union, Dict, Any
+
+# Configure logging
+logger = logging.getLogger(__name__)
+# Ensure no handlers are already attached to avoid duplication
+if not logger.hasHandlers():
+    log_formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
+
+    # Specify the log file path
+    log_file_path = 'app.log'  # Change this path as needed
+
+    # Set up FileHandler
+    log_handler = logging.FileHandler(log_file_path)
+    log_handler.setFormatter(log_formatter)
+    log_handler.setLevel(logging.INFO)
+
+    logger.setLevel(logging.INFO)
+    logger.addHandler(log_handler)
+    logger.propagate = False  # Prevent propagation to the root logger
 
 def get_ffprobe_data(udp_link: str) -> Union[Dict[str, Any], str]:
     """
@@ -17,31 +36,38 @@ def get_ffprobe_data(udp_link: str) -> Union[Dict[str, Any], str]:
     ]
 
     try:
+        logger.info(f"Running ffprobe command: {' '.join(ffprobe_cmd)}")
         result = subprocess.run(
-            ffprobe_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            ffprobe_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+        )
 
-        # Check if ffprobe returned an error
         if result.returncode != 0:
             error_message = result.stderr.strip()
+            logger.error(f"ffprobe error: {error_message}")
             if "ffprobe: command not found" in error_message:
                 return "Error: ffprobe is not installed or not found in PATH."
             return f"Error: ffprobe failed with error: {error_message}"
 
-        # Try to parse the output as JSON
         try:
-            return json.loads(result.stdout)
+            ffprobe_output = json.loads(result.stdout)
+            logger.info("Successfully parsed ffprobe output.")
+            return ffprobe_output
         except json.JSONDecodeError:
+            logger.error("Failed to parse ffprobe output as JSON.")
             return "Error: Unable to parse ffprobe output as JSON."
 
     except FileNotFoundError:
+        logger.error("ffprobe is not installed or not found in PATH.")
         return "Error: ffprobe is not installed or not found in PATH."
     except Exception as e:
+        logger.exception(f"Unexpected error occurred: {e}")
         return f"Error: An unexpected error occurred: {str(e)}"
 
 def construct_programs_dict(ffprobe_data: dict) -> dict:
     """
     Construct a dictionary of programs and their streams from ffprobe data.
     """
+    logger.info("Constructing programs dictionary from ffprobe data.")
     programs = {}
     for program in ffprobe_data.get("programs", []):
         program_num = program["program_id"]
@@ -63,18 +89,19 @@ def construct_programs_dict(ffprobe_data: dict) -> dict:
                 streams["audio"].append(
                     {"id": stream_id, "codec": codec_name, "selected": False}
                 )
-            # Additional stream types can be handled here
         programs[program_num] = {
             "title": title,
             "streams": streams,
-            "selected": False  # Initialize selected state to False for all streams in a program
+            "selected": False
         }
+    logger.info(f"Constructed programs dictionary with {len(programs)} programs.")
     return programs
 
 def construct_ffmpeg_command(udp_link: str, programs: dict, adapter_num: int, modulator_num: int, log_file: str) -> str:
     """
     Construct the ffmpeg command based on the selected programs and streams.
     """
+    logger.info("Constructing ffmpeg command.")
     udp_params = "?fifo_size=10000000&overrun_nonfatal=1&reconnect=1&reconnect_streamed=1&reconnect_delay_max=2"
     base_options = "-buffer_size 5000k -mpegts_flags +resend_headers+pat_pmt_at_frames+latm -pcr_period 20 -mpegts_copyts 1 -ignore_unknown -fflags +genpts+igndts -avoid_negative_ts make_zero"
     map_cmds = []
@@ -86,8 +113,7 @@ def construct_ffmpeg_command(udp_link: str, programs: dict, adapter_num: int, mo
             title = program_info["title"]
             streams = program_info["streams"]
             stream_map_indices = []
-            
-            # Map selected streams and collect stream indices for the program
+
             for stream_type, stream_list in streams.items():
                 for stream in stream_list:
                     if stream["selected"]:
@@ -95,8 +121,7 @@ def construct_ffmpeg_command(udp_link: str, programs: dict, adapter_num: int, mo
                         map_cmds.append(map_cmd)
                         stream_map_indices.append(f"st={stream_idx}")
                         stream_idx += 1
-            
-            # Construct program command
+
             program_cmd = f"-program program_num={program_num}:title=\"{title}\":{':'.join(stream_map_indices)}"
             program_cmds.append(program_cmd)
 
@@ -104,4 +129,5 @@ def construct_ffmpeg_command(udp_link: str, programs: dict, adapter_num: int, mo
                  f"{' '.join(map_cmds)} {' '.join(program_cmds)} -c copy "
                  f"-muxrate 31668449 -f mpegts -y /dev/dvb/adapter{adapter_num}/mod{modulator_num}")
 
+    logger.info(f"Constructed ffmpeg command: {final_cmd}")
     return final_cmd
