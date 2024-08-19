6e4dcc65c7127efde98350df7afd4c3250a709b5
Palii <paliydmn@gmail.com>
2024-08-19 14:49:00 +0300

    remove -c copy from test options

--- app/utils/ffmpeg_utils.py
+++ app/utils/ffmpeg_utils.py
@@ -142,7 +142,7 @@ def construct_ffmpeg_command(udp_link: str, programs: dict, adapter_num: int, mo
     #              f"-muxrate 31668449 -max_interleave_delta 0 -copyts -f mpegts -y /dev/dvb/adapter{adapter_num}/mod{modulator_num}")
     
     final_cmd = (f"ffmpeg  -copyts -start_at_zero -fflags +discardcorrupt+igndts+genpts -buffer_size 10000k -ignore_unknown -err_detect ignore_err -avoid_negative_ts make_zero -re -thread_queue_size 16384 -i \"{udp_link}{udp_params}\" {base_options} "
-                 f"{' '.join(map_cmds)} {' '.join(program_cmds)} -c copy "
+                 f"{' '.join(map_cmds)} {' '.join(program_cmds)} "
                  f" -c:v copy -c:a copy -c:s copy -muxrate 31668449 -max_interleave_delta 0 -mpegts_copyts 1 -fps_mode 0 -enc_time_base -1 -start_at_zero -copytb -1 -f mpegts -y /dev/dvb/adapter{adapter_num}/mod{modulator_num}")
 
     logger.info(f"Constructed ffmpeg command: \n{final_cmd}\n")
